a27d7a9fb92af61c30e9d2fad1d62b3d288b2907
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-07-02 17:16:12 -0400

    updated port deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const notes = require('./data/notes.json');
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/notes', (req, res) => {
@@ -12,7 +12,7 @@ app.get('/notes', (req, res) => {
     // return index.html
   })
 
-app.listen(3001, () => {
-    console.log('API server now on port 3001!');
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
 
